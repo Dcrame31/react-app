9b466a48ee1103e42ab47569b42a3931b76b74b8
Diane Crame <dcrame@icloud.com>
2020-09-27 14:52:21 -0700

    added two containers

---
+++ src/components/containers/PlantContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PlantContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default PlantContainer;
---
+++ src/components/containers/UserContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class UserContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default UserContainer;
